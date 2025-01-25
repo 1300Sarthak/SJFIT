653e52478764039206708ff505a4c522e023a28f
edwin yue <53625790+edwaddle@users.noreply.github.com>
2025-01-24 23:31:51 -0800

    early demo created
    The model is very buggy, so the tracker isn't too great. Had to make the up angle 50, and down angle 140 instead of 30 and 160 just to give the model more leniency. Also only works with left curl. Future plans to deploy it with react, and make it so there are multiple options to test out.

--- Fit.AI-TestWeb/script.js
+++ Fit.AI-TestWeb/script.js
@@ -14,6 +14,10 @@ const canvasElement = document.getElementById("output_canvas");
 const canvasCtx = canvasElement ? canvasElement.getContext("2d") : null;
 const enableWebcamButton = document.getElementById("webcamButton");
 const video = document.getElementById("webcam");
+const liveView = document.getElementById("liveView");
+
+const curlCounter = document.createElement("div");
+
 console.log(video.innerHTML + "working?");
 
 (async function createPoseLandmarker() {
@@ -35,33 +39,6 @@ console.log(video.innerHTML + "working?");
   });
   console.log("PoseLandmarker initialized");
 })();
-let count = 0;
-let countNum = 1;
-const calculate_angle = (a, b, c) => {
-  // Extract points
-  let aTemp = { x: a.x, y: a.y };
-  let bTemp = { x: b.x, y: b.y };
-  let cTemp = { x: c.x, y: c.y };
-
-  // Calculate radians using atan2
-  let radians =
-    Math.atan2(c.y - b.y, c.x - b.x) - Math.atan2(a.y - b.y, a.x - b.x);
-
-  // Convert radians to degrees
-  let angle = Math.abs((radians * 180.0) / Math.PI);
-
-  // Normalize angle to [0, 180]
-  if (angle > 180) {
-    angle = 360 - angle;
-  }
-
-  if (count === 50) {
-    console.log(angle);
-
-    count = 0;
-  }
-  count++;
-};
 
 // Enable Webcam
 if (navigator.mediaDevices?.getUserMedia) {
@@ -75,18 +52,22 @@ if (navigator.mediaDevices?.getUserMedia) {
     }
     webcamRunning = !webcamRunning;
     enableWebcamButton.textContent = webcamRunning
-      ? "DISABLE PREDICTIONS"
-      : "ENABLE PREDICTIONS";
+      ? "DISABLE TRACKER"
+      : "ENABLE TRACKER";
 
     if (webcamRunning) {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
       video.srcObject = stream;
       video.addEventListener("loadeddata", renderLoop);
+      updateCurlCounter("begin", 0);
     }
   });
 }
 
 let lastVideoTime = -1;
+let Langle = 0;
+let Lcounter = 0;
+let Lstage = "up";
 function renderLoop() {
   let startTimeMs = performance.now(); //performance in milliseconds
   if (webcamRunning) {
@@ -104,13 +85,24 @@ function renderLoop() {
           );
         });
       }
+
       try {
-        calculate_angle(
-          results.landmarks[0][11], //R- shoulder
-          results.landmarks[0][13], //R - elbow
-          results.landmarks[0][15] // R - wrist
-        );
+        let Lshoulder = results.landmarks[0][11];
+        let Lelbow = results.landmarks[0][13];
+        let Lwrist = results.landmarks[0][15];
+        Langle = calculate_angle(Lshoulder, Lelbow, Lwrist);
+        if (Langle > 140) {
+          Lstage = "down";
+          console.log("down ");
+          updateCurlCounter("down", Lcounter);
+        } else if (Langle < 50 && Lstage == "down") {
+          Lcounter++;
+          Lstage = "up";
+          updateCurlCounter("up", Lcounter);
+          console.log("up +1");
+        }
       } catch (TypeError) {
+        //if not detected, implement it so that it will stop the counter or print an alert or something
         console.log("Honk Honk hit the klaxon");
       }
 
@@ -122,33 +114,31 @@ function renderLoop() {
     });
   }
 }
-
-/*
-async function predictWebcam() {
-  if (poseLandmarker && canvasCtx && webcamRunning) {
-    //const startTimeMs = performance.now();
-    const videoTime = video.currentTime;
-
-    if (videoTime !== lastVideoTime) {
-      lastVideoTime = videoTime;
-
-      const results = await poseLandmarker.detectForVideo(
-        video, startTimeMs
-      );
-      canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-
-      if (results.landmarks) {
-        results.landmarks.forEach((landmark) => {
-          const drawingUtils = new DrawingUtils(canvasCtx);
-          drawingUtils.drawLandmarks(landmark);
-          drawingUtils.drawConnectors(
-            landmark,
-            PoseLandmarker.POSE_CONNECTIONS
-          );
-        });
-      }
-    }
-    requestAnimationFrame(predictWebcam);
+let count = 0;
+let countNum = 1;
+const calculate_angle = (a, b, c) => {
+  let aTemp = { x: a.x, y: a.y };
+  let bTemp = { x: b.x, y: b.y };
+  let cTemp = { x: c.x, y: c.y };
+  // Calculate radians using atan2
+  let radians =
+    Math.atan2(cTemp.y - bTemp.y, cTemp.x - bTemp.x) -
+    Math.atan2(aTemp.y - bTemp.y, aTemp.x - bTemp.x);
+  // Convert radians to degrees
+  let angle = Math.abs((radians * 180.0) / Math.PI);
+  // Normalize angle to [0, 180]
+  if (angle > 180) {
+    angle = 360 - angle;
   }
-}
-  */
+  if (count === 50) {
+    console.log(angle);
+    count = 0;
+  }
+  count++;
+  return angle;
+};
+
+const updateCurlCounter = (Lstage, Lcounter) => {
+  curlCounter.innerHTML = Lstage + "! Reps: " + Lcounter;
+  liveView.appendChild(curlCounter);
+};
